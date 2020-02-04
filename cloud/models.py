724f58e8cad77c0f445b64b5442383cc1293a54d
nineDays2019 <night_days@163.com>
2020-02-04 23:15:40 +0800

    [Add] File model

--- cloud/models.py
+++ cloud/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class File(models.Model):
+    create_time = models.fields.DateTimeField()
+    path = models.fields.CharField(max_length=1024)
+
